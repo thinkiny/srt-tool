8db303dfee5297148d4a27fafcb9dc876f519b9b
thinkiny <thinkiny@126.com>
2023-09-13 10:21:18 +0800

    replace srt in place

--- SrtTools.scala
+++ SrtTools.scala
@@ -7,7 +7,9 @@ import fs2.*
 import fs2.io.file.Files
 import fs2.io.file.Path
 
-case class AppOptions(input: String, output: Option[String] = None)
+case class AppOptions(
+    input: String
+)
 
 object SrtTools extends IOCaseApp[AppOptions]:
   def makeDirectory(p: Path): IO[Unit] =
@@ -15,25 +17,25 @@ object SrtTools extends IOCaseApp[AppOptions]:
       .exists(p)
       .ifM(IO.unit, Files[IO].createDirectory(p))
 
-  def run(options: AppOptions, arg: RemainingArgs): IO[ExitCode] =
-    val outputPath = options.output
-      .map(Path(_))
-      .getOrElse(Path(options.input).resolve("new_srt"))
+  def rewriteSrt(input: Path, backup: Path): IO[Unit] =
+    Files[IO].copy(input, backup) >>
+      Files[IO]
+        .readUtf8Lines(backup)
+        .fold(new SrtBuilder)((b, l) => b.addLine(l))
+        .flatMap(i => fs2.Stream.emits(i.mergeResult().zipWithIndex))
+        .map((item, i) => item.toSrtString(i + 1))
+        .through(Files[IO].writeUtf8(input))
+        .compile
+        .drain
 
-    makeDirectory(outputPath) >>
+  def run(options: AppOptions, arg: RemainingArgs): IO[ExitCode] =
+    val backupPath = Path(options.input).resolve("srt_bak")
+
+    makeDirectory(backupPath) >>
       Files[IO]
         .walk(Path(options.input), 1, false)
         .filter(_.toString.endsWith(".srt"))
-        .flatMap(p =>
-          Files[IO]
-            .readUtf8Lines(p)
-            .fold(new SrtBuilder)((b, l) => b.addLine(l))
-            .flatMap(i => fs2.Stream.emits(i.mergeResult().zipWithIndex))
-            .map((item, i) => item.toSrtString(i + 1))
-            .through(
-              Files[IO].writeUtf8(outputPath.resolve(p.fileName))
-            )
-        )
+        .evalMap(p => rewriteSrt(p, backupPath.resolve(p.fileName)))
         .compile
         .drain
         .as(ExitCode.Success)
